7e604e81341b820e34777bb70ab75ea67f2232e1
Felipe Nascimento <flpnascto@gmail.com>
2021-02-24 23:08:41 -0300

    Criar Repository para User

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UserRepository);
 
     // SELECT * FROM users WHERE email = "email" LIMIT 1;
     const userAlreadyExists = await usersRepository.findOne({
@@ -31,4 +31,4 @@ class UserController {
   }
 }
 
-export { UserController }
+export { UserController };
