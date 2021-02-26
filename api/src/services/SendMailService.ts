da95a7415a46d3b1a3772526d4a977274dcfcc2a
Felipe Nascimento <flpnascto@gmail.com>
2021-02-25 22:54:58 -0300

    Criar template de envio do email e adicinar ao SendEmailService

--- api/src/services/SendMailService.ts
+++ api/src/services/SendMailService.ts
@@ -1,4 +1,7 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { resolve } from 'path';
+import handlebars from 'handlebars';
+import fs from 'fs';
 
 class SendMailService {
   private client: Transporter;
@@ -20,10 +23,19 @@ class SendMailService {
       })
   }
   async execute(to: string, subject: string, body: string) {
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+    const templetaFileContente = fs.readFileSync(npsPath).toString("utf8");
+    const mailTemplateParse = handlebars.compile(templetaFileContente);
+    const html = mailTemplateParse({
+      name: to,
+      title: subject,
+      description: body
+    })
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
+      html,
       from: "NPS <noreplay@nps.com.br>"
 
     })
